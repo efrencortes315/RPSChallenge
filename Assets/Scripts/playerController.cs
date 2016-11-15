a26f9861c944f88dce74ac7d15b0688fb94b5296
efren <efren>
2016-11-14 20:09:54 -0500

    Initial commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("a")){
+			print("rock");
+
+		}
+		else if(Input.GetKey("s")){
+			print ("paper");
+		}
+		else if(Input.GetKey("d")){
+			print ("scissors");
+		}
+	}
+}
