788d21a3dc5178bd615a5eb481c8baf19f76eb1b
Efren Cortes <efren.cortes315@gmail.com>
2016-11-16 02:14:03 -0500

    11_16_2016

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,23 +2,63 @@
 using System.Collections;
 
 public class playerController : MonoBehaviour {
+	public int playerNumber;
+	public int score;
+
+	//11/15//
+	public weapons[] moves = new weapons[5];
+	public int curPosInMoves;
+
+	//11/15//
+	public enum weapons	
+	{
+		rock,
+		paper,
+		scissors,
+		none
+	}
+
+	public weapons curChoice;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		curPosInMoves = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey("a")){
-			print("rock");
+		if (playerNumber == 1) {
+			if (Input.GetKeyDown ("a")) {
+				curChoice=weapons.rock;
+				addToMoves();
+
+			} else if (Input.GetKeyDown ("s")) {
+				curChoice=weapons.paper;
+				addToMoves();
+			} else if (Input.GetKeyDown ("d")) {
+				curChoice=weapons.scissors;
+				addToMoves();
+			}
+		} else if (playerNumber == 2) {
+			if (Input.GetKeyDown("j")) {
+				curChoice=weapons.rock;
+				addToMoves();
+			} else if (Input.GetKeyDown ("k")) {
+				curChoice=weapons.paper;
+				addToMoves();
+			} else if (Input.GetKeyDown ("l")) {
+				curChoice=weapons.scissors;
+				addToMoves();
+			}
 
 		}
-		else if(Input.GetKey("s")){
-			print ("paper");
-		}
-		else if(Input.GetKey("d")){
-			print ("scissors");
+
+	}
+	void addToMoves(){
+		if(curPosInMoves<5){
+			moves[curPosInMoves]=curChoice;
+			curPosInMoves++;
 		}
 	}
 }
---
+++ Assets/match.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class match : MonoBehaviour {
+	public playerController player1;
+	public playerController player2;
+	public Text uiTimer;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("judge", 1f, 7f);
+		InvokeRepeating ("timer", 1f, 1f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void judge(){
+		/*if (player1.curChoice == playerController.weapons.rock && player2.curChoice == playerController.weapons.scissors) {
+			player1.score+=1;
+		}else if (player1.curChoice == playerController.weapons.paper && player2.curChoice == playerController.weapons.scissors) {
+			player2.score+=1;
+		}else if (player1.curChoice == playerController.weapons.rock && player2.curChoice == playerController.weapons.paper) {
+			player2.score+=1;
+		}else if (player1.curChoice == playerController.weapons.paper && player2.curChoice == playerController.weapons.rock) {
+			player1.score+=1;
+		}else if (player1.curChoice == playerController.weapons.scissors && player2.curChoice == playerController.weapons.paper) {
+			player1.score+=1;
+		}else if (player1.curChoice == playerController.weapons.scissors && player2.curChoice == playerController.weapons.rock) {
+			player2.score+=1;
+		}*/
+		//Array based combat
+		for (int i=0; i<player1.moves.Length; i++) {
+			//handle if player didnt input anything
+			if(player1.moves[i] == playerController.weapons.none){
+
+			}
+			if(player2.moves[i] == playerController.weapons.none){
+
+			}
+			//end handle input error
+
+			if (player1.moves [i] == playerController.weapons.rock && player2.moves[i] == playerController.weapons.scissors) {
+				player1.score+=1;
+			}else if (player1.moves [i] == playerController.weapons.paper && player2.moves [i] == playerController.weapons.scissors) {
+				player2.score+=1;
+			}else if (player1.moves [i] == playerController.weapons.rock && player2.moves [i] == playerController.weapons.paper) {
+				player2.score+=1;
+			}else if (player1.moves [i] == playerController.weapons.paper && player2.moves [i] == playerController.weapons.rock) {
+				player1.score+=1;
+			}else if (player1.moves [i] == playerController.weapons.scissors && player2.moves [i] == playerController.weapons.paper) {
+				player1.score+=1;
+			}else if (player1.moves [i] == playerController.weapons.scissors && player2.moves [i] == playerController.weapons.rock) {
+				player2.score+=1;
+			}
+		}
+		for (int i=0; i<player1.moves.Length; i++) {
+			player1.moves[i]=playerController.weapons.none;
+			player2.moves[i]=playerController.weapons.none;
+		}
+		player1.curPosInMoves = 0;
+		player2.curPosInMoves = 0;
+		//Array based combat end
+		print ("Player 1: " + player1.score + "/ Player 2: " + player2.score);
+	}
+	void timer(){
+
+		float a = Time.time % 7;
+		uiTimer.text = a.ToString();
+	}
+}
